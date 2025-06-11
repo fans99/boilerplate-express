2257a86929eb7c4fe59d9810ab11108cc38dfaee
Erlan <fans9901@163.com>
2025-06-11 21:29:21 +0800

    update

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 let express = require('express');
 let app = express();
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + "/views//index.html")
+  res.sendFile(__dirname + "/views//index.html");
 });
+
+app.use('/public', express.static(__dirname + '/public'));
 
 
 
