ef61bb8c289b91030fc6a0198583f0eb0e96a29b
Erlan <fans9901@163.com>
2025-06-11 21:40:41 +0800

    update

--- myApp.js
+++ myApp.js
@@ -1,12 +1,18 @@
 let express = require('express');
 let app = express();
+require('dotenv').config();
 app.get('/', function(req, res) {
   res.sendFile(__dirname + "/views//index.html");
 });
 
 app.get('/json', function(req, res) {
+  let mes;
+  if (process.env.MESSAGE_STYLE == 'uppercase') {
+    mes = 'HELLO JSON';
+  }
+  else { mes = 'Hellow json'; }
   res.json({
-    "message": "Hello json"
+    "message": mes
   });
 });
 
